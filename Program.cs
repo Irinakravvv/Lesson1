a43570fdc3da74f2c2bafe29e14994e36d67da1e
unknown <tvot21@gmail.com>
2022-09-01 13:40:56 +0300

    Квадрат

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+
+string input = Console.ReadLine(); // Ввод
+
+int number = int.Parse (input); //Число
+
+// if ( int.TryParse(input))
+
+int square = number * number; // квадрат
+
+Console.WriteLine($"Число в квадрате равно {square}");
